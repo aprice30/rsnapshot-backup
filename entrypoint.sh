9cb0b3180250ab9006e4fbaed70e4bd4f887dd57
Adam Price <aprice30@gmail.com>
2024-11-23 15:59:35 +0000

    Changed cron so we can pass in the CRON_SCHEDULE as an env var, defaults to 4am

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
+# Default schedule is 4 AM daily if CRON_SCHEDULE is not set
+CRON_SCHEDULE="${CRON_SCHEDULE:-0 4 * * *}"
+
 # Ensure the cron service is running
 service cron start
 
-# Write the cron job for the backup script
+# Write the cron job for the backup script with the provided schedule
 CRON_FILE="/etc/cron.d/backup-cron"
-echo "0 4 * * * root /usr/local/bin/rsnapshot-backup.sh >> /var/log/backup.log 2>&1" > $CRON_FILE
+echo "$CRON_SCHEDULE root /usr/local/bin/rsnapshot-backup.sh >> /var/log/backup.log 2>&1" > $CRON_FILE
 chmod 0644 $CRON_FILE
 crontab $CRON_FILE
 
