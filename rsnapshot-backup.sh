c25cb25fdf9064a95dc69ce9e2ca2a102f7534f4
Adam Price <aprice30@gmail.com>
2024-11-25 08:22:42 +0000

    Final change to rsync args which I think now work

--- rsnapshot-backup.sh
+++ rsnapshot-backup.sh
@@ -43,8 +43,8 @@ echo -e "snapshot_root\t/data/backups/" >> $TEMP_CONF
 echo -e "no_create_root\t1" >> $TEMP_CONF
 echo -e "cmd_rsync\t/usr/bin/rsync" >> $TEMP_CONF
 echo -e "sync_first\t1" >> $TEMP_CONF
-echo -e "use_lazy_deletes\t1" >> $TEMP_CONF
-echo -e "rsync_long_args\t--delete\t--numeric-ids\t--relative\t--delete-excluded\t--exclude=.sync\t--inplace\t--partial\t--no-owner\t--no-group" >> $TEMP_CONF
+echo -e "rsync_long_args\t--delete\t--numeric-ids\t--relative\t--delete-excluded\t--exclude=.sync\t--inplace\t--partial" >> $TEMP_CONF
+echo -e "rsync_short_args\t-rlptDv" >> $TEMP_CONF
 
 # Include container volumes in rsnapshot conf
 backup_points_added=false
